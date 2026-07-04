0bb81a034044ca61f668bda99033d0d59b1a8f12
Mark Lozano <mark.d.lozano@gmail.com>
2026-07-04 02:01:42 -0600

    chore: retire the throwaway-prototype ignore entries now the rollup productionizes the prose-rollup prototype

--- eslint.config.js
+++ eslint.config.js
@@ -2,8 +2,7 @@ import js from "@eslint/js";
 import tseslint from "typescript-eslint";
 
 export default tseslint.config(
-  // prototypes/ holds throwaway probes: never committed (.gitignore), never linted. Remove this entry when the prototype is deleted.
-  { ignores: ["**/node_modules/", "**/out/", "**/prototypes/"] },
+  { ignores: ["**/node_modules/", "**/out/"] },
   js.configs.recommended,
   ...tseslint.configs.recommended,
 );
